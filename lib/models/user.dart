74b347530441c24022c3abf09c206b0d5a0b6b96
Erick <hearteric57@gmail.com>
2023-05-15 22:15:44 +0300

    feat: Added custom user model for data manipulation

---
+++ lib/models/user.dart
@@ -0,0 +1,44 @@
+import 'package:hive/hive.dart';
+
+part 'user.g.dart';
+
+@HiveType(typeId: 1)
+class User {
+  @HiveField(0)
+  String? name;
+
+  @HiveField(1, defaultValue: 0.0)
+  double? gpa;
+
+  @HiveField(2)
+  String? admno;
+
+  @HiveField(3)
+  String? password;
+
+  // To json object
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'admno': admno,
+      'password': password,
+      'gpa': gpa,
+    };
+  }
+
+  // from json
+  User.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        gpa = json['gpa'],
+        password = json['password'],
+        admno = json['admno'];
+
+  Map<String, dynamic> toModel() {
+    return {
+      'admno': admno,
+      'password': password,
+    };
+  }
+
+  User(); // Explicitly does nothing to silence warnings
+}
