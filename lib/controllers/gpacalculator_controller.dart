c8f1c9fdeb14c08f1fe2f3cdfde68957ab6b6180
NewtonMutugi <newtonmut197@gmail.com>
2023-10-23 03:10:21 +0300

    feat: GPA Calculator finished major functionality add modified UI elements

---
+++ lib/controllers/gpacalculator_controller.dart
@@ -0,0 +1,78 @@
+import 'package:academia/models/courses.dart';
+import 'package:get/get.dart';
+
+class GPACalculatorController extends GetxController {
+  final RxList<Unit> _courses = <Unit>[].obs;
+
+  List<Unit> get courseList => _courses;
+  double get gpa => _calculateGPA();
+
+  void addCourse(String name, String code, double creditHours, String grade) {
+    final course =
+        Unit(name: name, code: code, creditHours: creditHours, grade: grade);
+    _courses.add(course);
+  }
+
+  void removeCourse(int index) {
+    _courses.removeAt(index);
+    _courses.refresh();
+  }
+
+  void updateCourse(
+      {required int index,
+      required String newName,
+      required String newCode,
+      required double newCreditHours,
+      required String newGrade}) {
+    _courses[index].name = newName;
+    _courses[index].code = newCode;
+    _courses[index].creditHours = newCreditHours;
+    _courses[index].grade = newGrade;
+    _courses.refresh();
+  }
+
+  double _calculateGPA() {
+    double totalPoints = 0;
+    num totalCreditHours = 0;
+    // TODO: Perform cases to calculate grades
+    for (var course in _courses) {
+      totalPoints += getGradePoints(course.grade) * course.creditHours;
+      totalCreditHours += course.creditHours;
+    }
+
+    return totalPoints / totalCreditHours;
+  }
+
+  static double getGradePoints(String grade) {
+    switch (grade) {
+      case 'A':
+        return 4.0;
+      case 'A-':
+        return 3.7;
+      case 'B+':
+        return 3.3;
+      case 'B':
+        return 3.0;
+      case 'B-':
+        return 2.7;
+      case 'C+':
+        return 2.3;
+      case 'C':
+        return 2.0;
+      case 'C-':
+        return 1.7;
+      case 'D+':
+        return 1.3;
+      case 'D':
+        return 1.0;
+      case 'D-':
+        return 0.7;
+      case 'E':
+        return 0.0;
+      case 'F':
+        return 0.0;
+      default:
+        return 0.0;
+    }
+  }
+}
---
+++ lib/pages/gpacalculator_page.dart
@@ -0,0 +1,320 @@
+import 'package:academia/controllers/gpacalculator_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class GpaCalculator extends StatelessWidget {
+  GpaCalculator({super.key});
+  final tabs = [
+    "Red",
+    "Orange",
+    "Green",
+  ];
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController codeController = TextEditingController();
+  final TextEditingController creditHoursController = TextEditingController();
+  final TextEditingController gradeController = TextEditingController();
+  final unitController = GPACalculatorController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text("GPA Calculator"),
+        // automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text(
+              "GPA Calculator",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Enter your grades and credit hours for each course and press Calculate to work out your GPA.",
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Get.defaultDialog(
+                  title: "Your GPA",
+                  content: Text("Your GPA is ${unitController.gpa}"),
+                );
+              },
+              child: const Text("Calculate GPA"),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Obx(() => ListView.builder(
+                      itemCount: unitController.courseList.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        final unit = unitController.courseList[index];
+                        return ListTile(
+                          title: Text(unit.name),
+                          subtitle: Text(unit.code),
+                          trailing: Text(unit.grade),
+                          onTap: () {
+                            nameController.text = unit.name;
+                            codeController.text = unit.code;
+                            creditHoursController.text =
+                                unit.creditHours.toString();
+                            gradeController.text = unit.grade;
+
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (context) => Flex(
+                                direction: Axis.vertical,
+                                children: [
+                                  Expanded(
+                                    child: SingleChildScrollView(
+                                      child: Column(
+                                        children: [
+                                          const SizedBox(
+                                            height: 20,
+                                          ),
+                                          const Text(
+                                            "Edit the unit details",
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: 'Unit Name',
+                                              ),
+                                              controller: nameController,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: 'Unit Code',
+                                              ),
+                                              controller: codeController,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: 'Credit Hours',
+                                              ),
+                                              controller: creditHoursController,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: const InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                labelText: 'Grade',
+                                              ),
+                                              controller: gradeController,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Row(
+                                            children: [
+                                              Flexible(
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: ElevatedButton(
+                                                    onPressed: () {
+                                                      unitController.updateCourse(
+                                                          index: index,
+                                                          newName:
+                                                              nameController
+                                                                  .text,
+                                                          newCode:
+                                                              codeController
+                                                                  .text,
+                                                          newCreditHours:
+                                                              double.parse(
+                                                                  creditHoursController
+                                                                      .text),
+                                                          newGrade:
+                                                              gradeController
+                                                                  .text);
+                                                      Get.back();
+                                                    },
+                                                    child:
+                                                        const Text("Edit unit"),
+                                                  ),
+                                                ),
+                                              ),
+                                              Flexible(
+                                                child: Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: ElevatedButton(
+                                                    onPressed: () {
+                                                      unitController
+                                                          .removeCourse(index);
+                                                      Get.back();
+                                                    },
+                                                    child: const Text(
+                                                        "Remove Unit"),
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    )),
+              ),
+            ),
+            FloatingActionButton(
+                onPressed: () {
+                  nameController.text = "";
+                  codeController.text = "";
+                  creditHoursController.text = "";
+                  gradeController.text = "";
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Flex(
+                      direction: Axis.vertical,
+                      children: [
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                const Text(
+                                  "Enter the unit details",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Unit Name',
+                                    ),
+                                    controller: nameController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Unit Code',
+                                    ),
+                                    controller: codeController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Credit Hours',
+                                    ),
+                                    controller: creditHoursController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Grade',
+                                    ),
+                                    controller: gradeController,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    final name = nameController.text;
+                                    final code = codeController.text;
+                                    final creditHours = double.parse(
+                                        creditHoursController.text);
+                                    final grade = gradeController.text;
+                                    if (nameController.text != '' &&
+                                        codeController.text != '' &&
+                                        creditHoursController.text != '' &&
+                                        gradeController.text != '') {
+                                      unitController.addCourse(
+                                          name, code, creditHours, grade);
+                                      Get.back();
+                                    }
+                                  },
+                                  child: const Text("Add unit"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: const Icon(Icons.add))
+          ],
+        ),
+      ),
+    );
+  }
+}
