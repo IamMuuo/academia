d20b4ed100d89c46ef1cdba3ceb4e537392798d2
Erick <hearteric57@gmail.com>
2023-12-29 23:11:54 +0300

    feat: toolpage search bar working

--- lib/pages/tool_page.dart
+++ lib/pages/tool_page.dart
@@ -1,3 +1,4 @@
+import 'package:academia/widgets/tool_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,69 @@ class ToolsPage extends StatefulWidget {
 
 class _ToolsPageState extends State<ToolsPage> {
   // all available tools
+  final List<Map<String, dynamic>> _allTools = [
+    {
+      "id": 1,
+      "name": "GPA Calculator",
+      "action": "Calculate GPA",
+      "description": "Wanna calculate your GPA? try it here"
+    },
+    {
+      "id": 2,
+      "name": "Generate Catering Token",
+      "action": "Generate Token",
+      "description": "Hungry? Maybe its time to generate your catering token!"
+    },
+    {
+      "id": 3,
+      "name": "Elearning",
+      "action": "Visit Elearning",
+      "description":
+          "Psst! Elearning is here for you. Keep track of your assignments and notes!",
+    },
+    {
+      "id": 4,
+      "name": "Class Attendance",
+      "action": "View class Attendance",
+      "description":
+          "Curious to know how many classes you have missed this semester, this might be the tool",
+    },
+    {
+      "id": 5,
+      "name": "Exam Timetable",
+      "action": "Show exam timetable",
+      "description":
+          "Exams around the corner? Don't panic we've got you covered with the timetable",
+    },
+  ];
+
+  List<Map<String, dynamic>> foundTools = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // At the beginning all tools are visible
+    foundTools = _allTools;
+  }
+
+  void _runToolFilter(String enteredKeyWord) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyWord.isEmpty) {
+      results = _allTools;
+    } else {
+      results = _allTools
+          .where((element) => element["name"]
+              .toString()
+              .toLowerCase()
+              .contains(enteredKeyWord.toLowerCase()))
+          .toList();
+    }
+
+    // refresh the ui
+    setState(() {
+      foundTools = results;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +91,7 @@ class _ToolsPageState extends State<ToolsPage> {
               width: MediaQuery.of(context).size.width,
               height: 60,
               child: TextField(
+                onChanged: (value) => _runToolFilter(value),
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(8)),
@@ -41,6 +106,32 @@ class _ToolsPageState extends State<ToolsPage> {
               ),
             ),
             const SizedBox(height: 12),
+            Expanded(
+              child: foundTools.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: foundTools.length,
+                      itemBuilder: (context, index) {
+                        return ToolCard(
+                          heading: foundTools[index]["name"],
+                          description: foundTools[index]["description"],
+                          image: "assets/images/bot_sad.png",
+                          ontap: () {},
+                          action: foundTools[index]["action"],
+                        );
+                      },
+                    )
+                  : Column(
+                      children: [
+                        const Text(
+                          "We can't find that requested tool at the moment. Please try using alternative keywords or",
+                          textAlign: TextAlign.center,
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text("Tap to request this feature")),
+                      ],
+                    ),
+            ),
           ],
         ),
       ),
--- lib/widgets/tool_card.dart
+++ lib/widgets/tool_card.dart
@@ -1,83 +1,59 @@
-import 'package:academia/constants/common.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class ToolCard extends StatefulWidget {
   const ToolCard({
     super.key,
+    required this.heading,
+    required this.image,
+    required this.description,
     required this.ontap,
-    required this.title,
-    this.backGround,
-    this.icon,
-    this.titlestyle,
+    required this.action,
   });
-
+  final String heading;
+  final String image;
+  final String description;
   final Function ontap;
-  final Color? backGround;
-  final Icon? icon;
-  final String title;
-  final TextStyle? titlestyle;
+  final String action;
 
   @override
   State<ToolCard> createState() => _ToolCardState();
 }
 
 class _ToolCardState extends State<ToolCard> {
-  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: widget.backGround ?? Theme.of(context).primaryColor,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(12),
+    return Card(
+      elevation: 4.0,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(widget.heading),
+            // subtitle: Text(subheading),
+            trailing: const Icon(Icons.favorite_outline),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                widget.icon ??
-                    const Icon(
-                      CupertinoIcons.hammer_fill,
-                      color: Colors.white,
-                    ),
-                isLoading
-                    ? LoadingAnimationWidget.hexagonDots(
-                        size: 20, color: Colors.white)
-                    : IconButton(
-                        onPressed: () async {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          await widget.ontap();
-                          setState(() {
-                            isLoading = false;
-                          });
-                        },
-                        icon: const Icon(
-                          CupertinoIcons.arrow_right_circle,
-                          color: Colors.white,
-                        ),
-                      ),
-              ],
+          SizedBox(
+            height: 200.0,
+            child: Image.asset(
+              widget.image,
+              fit: BoxFit.cover,
             ),
-            Text(
-              widget.title,
-              style: widget.titlestyle ??
-                  h6.copyWith(
-                    fontSize: 14,
-                    color: Colors.white,
-                  ),
-            )
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            alignment: Alignment.centerLeft,
+            child: Text(widget.description),
+          ),
+          ButtonBar(
+            children: [
+              TextButton(
+                child: Text(widget.action),
+                onPressed: () {
+                  widget.ontap.call();
+                },
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
