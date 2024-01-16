8bd3cdd5e04dfa7c5523301f377e1cf73ec3900b
Erick <hearteric57@gmail.com>
2024-01-16 12:13:00 +0300

    chore: added birthday page

---
+++ lib/pages/birthday_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BirthDayPage extends StatelessWidget {
+  const BirthDayPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Happy BirthDay"),
+      ),
+    );
+  }
+}
