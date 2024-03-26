77cb2ed85e980b312604941348ebf03ffafa329a
Erick <hearteric57@gmail.com>
2024-03-26 22:59:00 +0300

    chore: exam timetable basic ui

--- lib/tools/exam_timetable/exams_timetable_controller.dart
+++ lib/tools/exam_timetable/exams_timetable_controller.dart
@@ -1,61 +1,26 @@
-import 'package:academia/constants/common.dart';
-import 'package:academia/models/courses.dart';
-import 'package:flutter/material.dart';
+import 'package:academia/exports/barrel.dart';
 import 'package:get/get.dart';
 
 class ExamsTimeTableController extends GetxController {
-  var hasExams = false.obs;
-  var isLoading = false.obs;
-
-  Future<List<dynamic>> fetchExamTimeTable(String units,
-      {bool athi = true}) async {
-    isLoading.value = true;
-
-    try {
-      var fetchedUnits = await magnet.fetchExamTimeTabale(units);
-      isLoading.value = false;
-
-      return fetchedUnits;
-    } catch (e) {
-      Get.snackbar(
-        "Oh Snap!",
-        "Something went wrong while attempting to fetch your exam timetable, please check your network connection and try again",
-        icon: const Icon(
-          Icons.network_ping,
-        ),
-        maxWidth: 500,
-      );
-      debugPrint(e.toString());
-    }
-
-    isLoading.value = false;
-    return [];
+  Future<void> fetchExams() async {
+    await Future.delayed(const Duration(seconds: 10));
+    return;
   }
 
-  Future<void> addFetchedUnits(List<dynamic> fetchedUnits) async {
-    await appDB.put("exam_timetable", fetchedUnits);
-  }
-
+  List<Courses> userCourses = <Courses>[];
   @override
-  void onInit() async {
-    debugPrint(appDB.get("exam_timetable").toString());
-
-    if (true) {
-      var courses = appDB.get("timetable");
-      String payload = "";
-      for (Courses c in courses) {
-        payload =
-            "$payload ${c.name!.replaceAll("-", " ")}${c.section!.split('-')[0]},";
-      }
-
-      debugPrint(payload);
-      var fetchedUnits = await fetchExamTimeTable(payload.trim());
-      await addFetchedUnits(fetchedUnits);
-      hasExams.value = fetchedUnits.isNotEmpty;
-    }
-
-    debugPrint("do I have exams : ${hasExams.value}");
-
+  Future<void> onInit() async {
     super.onInit();
+    // Load user courses
+    debugPrint(appDB.keys.toString());
+    final List coursesData = await appDB.get("timetable");
+    coursesData.forEach((element) {
+      if (element != null) {
+        userCourses.add(element);
+      }
+    });
+    debugPrint("User Courses Fetched");
+    // userCourses =
+    //     courses.map((e) => Courses.fromJson(e as Map)).toList().cast<Courses>();
   }
 }
--- lib/tools/exam_timetable/exams_timetable_page.dart
+++ lib/tools/exam_timetable/exams_timetable_page.dart
@@ -1,28 +1,171 @@
 import 'package:academia/exports/barrel.dart';
+import 'package:academia/tools/exam_timetable/exams_timetable_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:get/get.dart';
 
-class ExamTimeTablePage extends StatelessWidget {
+class ExamTimeTablePage extends StatefulWidget {
   const ExamTimeTablePage({super.key});
+
+  @override
+  State<ExamTimeTablePage> createState() => _ExamTimeTablePageState();
+}
+
+class _ExamTimeTablePageState extends State<ExamTimeTablePage> {
+  final userController = Get.find<UserController>();
+  final settingsController = Get.find<SettingsController>();
+  final examtimetableController = Get.put(ExamsTimeTableController());
+  final _searchController = TextEditingController();
+
+  bool _isSearching = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Image.asset("assets/images/girl_sitted.png"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(
+              "Hi ${userController.user.value!.name!.split(' ')[0].title()}",
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Ionicons.help),
+              ),
+            ],
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 60),
+                    const Spacer(),
+                    Text(
+                      "The important thing is not to stop questioning. Curiosity has its own reason for existing",
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            fontFamily: GoogleFonts.figtree().fontFamily,
+                          ),
+                    ),
+                    const Text(
+                      "~ Albert Einsten",
+                      textAlign: TextAlign.end,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          const Text("Exam timetable coming soon😉"),
-          TextButton(
-            onPressed: () {
-              Get.back();
-            },
-            child: const Text("Notify me when its ready"),
-          ),
+          SliverFillRemaining(
+            hasScrollBody: true,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Seems you have no exams at the moment",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          fontFamily: GoogleFonts.figtree().fontFamily,
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            isDismissible: false,
+            context: context,
+            builder: (context) => StatefulBuilder(
+              builder: (context, StateSetter setState) {
+                return Container(
+                  padding: const EdgeInsets.all(16),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          hintText: "BIL 111K, ENG 112, ...",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                          suffixIcon: IconButton(
+                            onPressed: () async {
+                              if (_searchController.text.trim().isEmpty) {
+                                showCustomSnackbar(
+                                  "Error",
+                                  "Please provide units to search",
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .errorContainer,
+                                  colorText:
+                                      Theme.of(context).colorScheme.onSurface,
+                                );
+                                return;
+                              }
+                              print("Here");
+                              if (mounted) {
+                                setState(() {
+                                  _isSearching = true;
+                                });
+
+                                await examtimetableController.fetchExams();
+                                if (!mounted) {
+                                  return;
+                                }
+                                setState(() {
+                                  _isSearching = false;
+                                });
+                              }
+                            },
+                            icon: const Icon(Ionicons.search),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      _isSearching
+                          ? const Text("Loading")
+                          : Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4),
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .tertiaryContainer,
+                              ),
+                              child: Text(
+                                "Please input your units seperated with commas and wait for the magic to happen🪄",
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleLarge!
+                                    .copyWith(
+                                      fontFamily:
+                                          GoogleFonts.figtree().fontFamily,
+                                    ),
+                              ),
+                            ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        },
+        child: const Icon(
+          Ionicons.add,
+        ),
       ),
     );
   }
