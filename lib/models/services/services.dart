e19ef7ee40adb7e21c06532348430e16e33c897c
Erick <hearteric57@gmail.com>
2024-06-24 18:29:28 +0300

    feat: new server integration

--- lib/models/services/services.dart
+++ lib/models/services/services.dart
@@ -4,5 +4,6 @@ export 'rewards_service.dart';
 /// The verisafe service provides the base url
 /// for user authentication
 mixin VerisafeService {
-  static const String urlPrefix = "http://192.168.100.2:8000";
+  // static const String urlPrefix = "http://192.168.100.2:8000";
+  static const String urlPrefix = "http://54.89.208.172:8000";
 }
