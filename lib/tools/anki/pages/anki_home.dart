91d11ecf47c0db08db2bfdaa64e9e5a5a3297df5
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-28 02:02:04 +0300

    feat: added grid view to list all topics

--- lib/tools/anki/pages/anki_home.dart
+++ lib/tools/anki/pages/anki_home.dart
@@ -1,15 +1,82 @@
-import 'package:flutter/material.dart';
+import 'package:academia/exports/barrel.dart';
+import 'package:academia/tools/anki/widgets/widgets.dart' show test_topics;
+import 'package:get/get.dart';
 
 class AnkiHomePage extends StatelessWidget {
   const AnkiHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome to Start Of Anki",
-        ),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ElevatedButton(
+            onPressed: () => Get.back(),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+            ),
+            child: const Icon(
+              Icons.arrow_back,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              "Starred Topics",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.29,
+            color: Colors.black,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () => debugPrint("Feature Coming Real Soon"),
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                ),
+                child: const Text(
+                  "Create Topic",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.52,
+            decoration: BoxDecoration(
+              color: lightColorScheme.primaryContainer,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(28),
+                topRight: Radius.circular(28),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(22.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, idx) {
+                  return test_topics[idx];
+                },
+                itemCount: test_topics.length,
+                scrollDirection: Axis.vertical,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
