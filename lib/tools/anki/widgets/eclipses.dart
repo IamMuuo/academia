4b6998afa2b2ee06ad3aa51d0e6677a4dd7af099
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-07-05 16:59:45 +0300

    feat: added extracircles

--- lib/tools/anki/widgets/eclipses.dart
+++ lib/tools/anki/widgets/eclipses.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:academia/exports/barrel.dart';
 
-Widget circle = Padding(
+Widget blackcircle = Padding(
   padding: const EdgeInsets.all(1.0),
   child: Container(
     width: 15,
@@ -8,6 +8,42 @@ Widget circle = Padding(
     decoration: const BoxDecoration(
       shape: BoxShape.circle,
       color: Color(0xff444c55),
+    ),
+  ),
+);
+
+Widget bluecircle = Padding(
+  padding: const EdgeInsets.all(1.0),
+  child: Container(
+    width: 15,
+    height: 15,
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      color: lightColorScheme.primaryContainer,
+    ),
+  ),
+);
+
+Widget redcircle = Padding(
+  padding: const EdgeInsets.all(1.0),
+  child: Container(
+    width: 15,
+    height: 15,
+    decoration: const BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffffcde5),
+    ),
+  ),
+);
+
+Widget greencircle = Padding(
+  padding: const EdgeInsets.all(1.0),
+  child: Container(
+    width: 15,
+    height: 15,
+    decoration: const BoxDecoration(
+      shape: BoxShape.circle,
+      color: Color(0xffe5ffcd),
     ),
   ),
 );
@@ -22,9 +58,28 @@ class CustomEclipse extends StatelessWidget {
       height: MediaQuery.of(context).size.height * 0.05,
       child: Row(
         children: [
-          circle,
-          circle,
-          circle,
+          blackcircle,
+          blackcircle,
+          blackcircle,
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMixEclipse extends StatelessWidget {
+  const CustomMixEclipse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.2,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: Row(
+        children: [
+          bluecircle,
+          redcircle,
+          greencircle,
         ],
       ),
     );
