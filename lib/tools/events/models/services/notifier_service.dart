ad910661c2331efa85783ede0791b84a504d0df3
Erick <hearteric57@gmail.com>
2024-07-21 01:45:45 +0300

    chore: moved notifier service to use staging server url

--- lib/tools/events/models/services/notifier_service.dart
+++ lib/tools/events/models/services/notifier_service.dart
@@ -1,4 +1,4 @@
 mixin NotifierService {
-  // final String notifierUrlPrefix = "http://notifier.erick.serv00.net";
-  final String notifierUrlPrefix = "http://localhost:8000";
+  final String notifierUrlPrefix = "http://notifier.erick.serv00.net";
+  // final String notifierUrlPrefix = "http://localhost:8000";
 }
