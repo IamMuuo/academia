e4ac4b3024fd38bfbef1ca7f4ee0e4e8d18864af
Eugene600 <ugowach160@gmail.com>
2024-08-05 21:38:52 +0300

    Workedon Score Screen More work needs to be done on this screen s well as the image needs replacing

--- lib/tools/ask_me/pages/scoreSection.dart
+++ lib/tools/ask_me/pages/scoreSection.dart
@@ -1,119 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class ScoreSection extends StatelessWidget {
-//   const ScoreSection({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         children: [
-//           Expanded(
-//             flex: 2,
-//             child: Image.asset("assets/images/congratulations_askMe.jpeg"),
-//           ),
-//           Expanded(
-//             flex: 3,
-//             child: Stack(
-//               alignment: Alignment.topCenter,
-//               children: [
-//                 Positioned.fill(
-//                   child: Column(
-//                     children: [
-//                       Expanded(
-//                         flex: 2,
-//                         child: Container(
-//                           color: Colors.white,
-//                         ),
-//                       ),
-//                       Expanded(
-//                         flex: 1,
-//                         child: Container(
-//                           color: const Color(0xFFB9D9EB),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 Container(
-//                   padding: const EdgeInsets.all(20),
-//                   margin: const EdgeInsets.symmetric(horizontal: 16.0),
-//                   decoration: BoxDecoration(
-//                     color: Colors.white,
-//                     borderRadius: BorderRadius.circular(20),
-//                     boxShadow: [
-//                       BoxShadow(
-//                         color: Colors.grey.withOpacity(0.5),
-//                         spreadRadius: 5,
-//                         blurRadius: 7,
-//                         offset: const Offset(0, 3),
-//                       ),
-//                     ],
-//                   ),
-//                   child: Column(
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       const Text(
-//                         "Congratulations",
-//                         style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-//                       ),
-//                       const SizedBox(height: 8),
-//                       const Text(
-//                         "You completed the test!",
-//                         style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-//                       ),
-//                       const SizedBox(height: 20),
-//                       Text(
-//                         "your score".toUpperCase(),
-//                         style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//                       ),
-//                       const SizedBox(height: 10),
-//                       const Text(
-//                         "3/10",
-//                         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//                       ),
-//                       const SizedBox(height: 10),
-//                       const Icon(
-//                         Icons.sentiment_dissatisfied,
-//                         color: Colors.yellow,
-//                         size: 40,
-//                       ),
-//                       const SizedBox(height: 10),
-//                       const Text(
-//                         "You can do better",
-//                         style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//           Container(
-//             height: 100,
-//             color: const Color(0xFFB9D9EB),
-//             child: Align(
-//               alignment: Alignment.bottomCenter,
-//               child: Padding(
-//                 padding: const EdgeInsets.all(16.0),
-//                 child: ElevatedButton(
-//                   onPressed: () {
-//                     // Implement later
-//                   },
-//                   style: ElevatedButton.styleFrom(
-//                     padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-//                   ),
-//                   child: const Text("Complete"),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class ScoreSection extends StatelessWidget {
@@ -122,20 +6,46 @@ class ScoreSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("The End"),
+      // ),
       body: Column(
         children: [
           Expanded(
-            flex: 1,
-            child: Image.asset('assets/images/congratulations_askMe.jpeg',),
+            flex: 2,
+            child: Image.asset('assets/images/congratulations_askMe.jpeg', 
+            width: 390,
+            fit: BoxFit.cover,
+            ),
           ),
           Expanded(
-            flex: 1,
+            flex: 3,
             child: Stack(
               alignment: Alignment.center,
               children: [
+                Positioned.fill(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          //color: const Color(0xFFB9D9EB),
+                          color: const Color(0xFF006399),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: const Color(0xFF006399),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
                 Container(
                   padding: const EdgeInsets.all(20),
                   margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                  width: 500,
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(20),
@@ -148,22 +58,60 @@ class ScoreSection extends StatelessWidget {
                       ),
                     ],
                   ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Congratulations",
+                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      const Text(
+                        "You completed the test!",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "your score".toUpperCase(),
+                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "3/10",
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      const Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: Colors.yellow,
+                        size: 40,
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "You can do better",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              color: const Color(0xFF006399),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {}, 
-                    child: const Text("Complete"),
+          Container(
+            height: 100,
+            color: const Color(0xFF006399),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Implement later
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
                   ),
+                  child: const Text("Complete"),
                 ),
               ),
             ),
@@ -173,3 +121,131 @@ class ScoreSection extends StatelessWidget {
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+
+// class ScoreSection extends StatelessWidget {
+//   const ScoreSection({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         children: [
+//           Image.asset(
+//             'assets/images/congratulations_askMe.jpeg',
+//             width: double.infinity,
+//             height: MediaQuery.of(context).size.height * 0.35,
+//             fit: BoxFit.cover,
+//           ),
+//           Stack(
+//             alignment: Alignment.center,
+//             children: [
+//               Column(
+//                 children: [
+//                   Container(
+//                     height: MediaQuery.of(context).size.height * 0.2,
+//                     color: Colors.white,
+//                   ),
+//                   Container(
+//                     height: MediaQuery.of(context).size.height * 0.3,
+//                     color: const Color(0xFFB9D9EB),
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 padding: const EdgeInsets.all(20),
+//                 margin: const EdgeInsets.symmetric(horizontal: 16.0),
+//                 decoration: BoxDecoration(
+//                   color: Colors.white,
+//                   borderRadius: BorderRadius.circular(20),
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color: Colors.grey.withOpacity(0.5),
+//                       spreadRadius: 5,
+//                       blurRadius: 7,
+//                       offset: const Offset(0, 3),
+//                     ),
+//                   ],
+//                 ),
+//                 child: Column(
+//                   mainAxisSize: MainAxisSize.min,
+//                   children: [
+//                     const Text(
+//                       "Congratulations",
+//                       style: TextStyle(
+//                           fontSize: 22,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black),
+//                     ),
+//                     const SizedBox(height: 8),
+//                     const Text(
+//                       "You completed the test!",
+//                       style: TextStyle(
+//                           fontSize: 18,
+//                           fontWeight: FontWeight.w300,
+//                           color: Colors.black),
+//                     ),
+//                     const SizedBox(height: 20),
+//                     Text(
+//                       "your score".toUpperCase(),
+//                       style: const TextStyle(
+//                           fontSize: 18,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black),
+//                     ),
+//                     const SizedBox(height: 10),
+//                     const Text(
+//                       "3/10",
+//                       style: TextStyle(
+//                           fontSize: 24,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black),
+//                     ),
+//                     const SizedBox(height: 10),
+//                     const Icon(
+//                       Icons.sentiment_dissatisfied,
+//                       color: Colors.yellow,
+//                       size: 40,
+//                     ),
+//                     const SizedBox(height: 10),
+//                     const Text(
+//                       "You can do better",
+//                       style: TextStyle(
+//                           fontSize: 18,
+//                           fontWeight: FontWeight.w300,
+//                           color: Colors.black),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Container(
+//             color: const Color(0xFFB9D9EB),
+//             child: Align(
+//               alignment: Alignment.bottomCenter,
+//               child: Padding(
+//                 padding: const EdgeInsets.all(16.0),
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     // Implement later
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     padding: const EdgeInsets.symmetric(
+//                         horizontal: 32, vertical: 16),
+//                     backgroundColor: Colors.white,
+//                     foregroundColor: Colors.black,
+//                   ),
+//                   child: const Text("Complete"),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
