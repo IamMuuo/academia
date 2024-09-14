74cc7f71994045d8c3cb8f59c896b39c844da8a5
Eugene600 <ugowach160@gmail.com>
2024-09-14 22:30:27 +0300

    Applied separation of concerns by creating ask me controller and also made a few tweaks as well on existing controllers to bring consistency.

--- lib/tools/ask_me/controllers/quiz_settings_controller.dart
+++ lib/tools/ask_me/controllers/quiz_settings_controller.dart
@@ -4,7 +4,6 @@ class QuizSettingsController extends GetxController {
   var filePath = ''.obs;
   var questionType = 'Multiple choice'.obs;
   var multipleChoice = true.obs;
-  var selectedTimer = 2.obs;
   var minute = 2.obs;
   var seconds = 30.obs;
 
@@ -18,10 +17,6 @@ class QuizSettingsController extends GetxController {
   }
   void setFilePath(String path) {
     filePath.value = path;
-  }
-
-  void setSelectedTimer(int timer) {
-    selectedTimer.value = timer;
   }
 
   void setTimer(int min, sec) {
--- lib/tools/ask_me/models/core/multiple_choice.dart
+++ lib/tools/ask_me/models/core/multiple_choice.dart
@@ -26,23 +26,3 @@ class MultipleChoiceQuestion{
   }
 }
 
-//Multiple choice Quiz model
-class MultipleChoiceQuiz {
-  List<MultipleChoiceQuestion> questions;
-
-  MultipleChoiceQuiz({required this.questions});
-
-  factory MultipleChoiceQuiz.fromJson(Map<String, dynamic> json) {
-    return MultipleChoiceQuiz(
-      questions: List<MultipleChoiceQuestion>.from(
-        json['questions'].map((questionJson) => MultipleChoiceQuestion.fromJson(questionJson)),
-      ),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'questions': questions.map((question) => question.toJson()).toList(),
-    };
-  }
-}
