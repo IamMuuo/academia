44c16c413dcff718259a6a780e333d0ed2e506fa
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-09-24 15:02:10 +0300

    fix: overflows on login page

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -70,6 +70,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         minimum: const EdgeInsets.all(12),
         child: Form(
